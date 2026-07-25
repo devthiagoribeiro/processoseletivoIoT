fb348537052d909a39fc83c746cc8badf094b586
thiagoribeiro <thiagordlr@gmail.com>
2026-07-25 13:11:43 +0000

    ajustar o nível lógico do botão pressionado

--- src/main.py
+++ src/main.py
@@ -24,18 +24,18 @@ print("Contador de Producao Inicializado")
 while True:
     # Leitura dos sensores
     adc_val = ldr.read()
-    btn_val = btn.value()
+    btn_val = not (btn.value())
 
     # Lógica de Reset do Turno
-    if btn_val == 0:
+    if btn_val == 1:
         time.sleep_ms(50)
-        if btn.value() == 0:
+        if btn.value() == 1:
             total_pecas = 0
             peca_bloqueando = False
             alerta_emitido = False
             print("Turno resetado com sucesso. Contadores zerados.")
             # Debounce do botão: espera até que o botão seja liberado
-            while btn.value() == 0:
+            while btn.value() == 1:
                 time.sleep_ms(20)
     
     # Avalia se a peça está na frente (bloqueando a luz)
@@ -52,7 +52,7 @@ while True:
         total_pecas += 1
         print(f"Peca detectada! Total: {total_pecas}")
         
-    # B. Lógica de Micro-paradas (Cronômetro Não-Bloqueante)
+    # Lógica de Micro-paradas (Cronômetro Não-Bloqueante)
     if peca_bloqueando and not alerta_emitido:
         tempo_passado = time.ticks_diff(time.ticks_ms(), tempo_inicio_bloqueio)
         if tempo_passado >= TEMPO_MICRO_PARADA:
